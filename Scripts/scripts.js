dc4f4aea5f936c976d735d7ebcf6d2c4abaf33fe
Ryan Kiratu <ryankiratu67@email.com>
2025-01-24 08:01:35 +0300

    new changes

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,16 +1,1 @@
-document
-  .getElementById("userForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission
 
-    const name = document.getElementById("name").value;
-    const yearOfBirth = document.getElementById("yearOfBirth").value;
-
-    if (name && yearOfBirth) {
-      const output = `Hello, ${name}! You were born in ${yearOfBirth}.`;
-      document.getElementById("output").textContent = output;
-    } else {
-      document.getElementById("output").textContent =
-        "Please fill in all fields.";
-    }
-  });
